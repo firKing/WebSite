d90b160a822757449e0010dfaec3a7ed1ab73456
zgf <zgf_lawliet@foxmail.com>
2015-01-27 09:45:43 +0800

    修改index传参

--- WebSite/Controllers/IndexController.cs
+++ WebSite/Controllers/IndexController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Web.Mvc;
+using WebSite.Controllers.Common;
 using WebSite.Controllers.Module;
 using WebSite.Models;
 
@@ -8,6 +9,53 @@ namespace WebSite.Controllers
 {
     public class IndexController : Controller
     {
+        private Pair<String,int> GetMonthAndDay(System.DateTime time)
+        {
+            var result = new Pair<String, int>();
+            result .second = time.Day); 
+            switch (time.Month)
+            {
+                case 1:
+                    result.first = "一月";
+                    break;
+                case 2:
+                    result.first = "二月";
+                    break;
+                case 3:
+                    result.first = "三月";
+                    break;
+                case 4:
+                    result.first = "四月";
+                    break;
+                case 5:
+                    result.first = "五月";
+                    break;
+                case 6:
+                    result.first = "六月";
+                    break;
+                case 7:
+                    result.first = "七月";
+                    break;
+                case 8:
+                    result.first = "八月";
+                    break;
+                case 9:
+                    result.first = "九月";
+                    break;
+                case 10:
+                    result.first = "十月";
+                    break;
+                case 11:
+                    result.first = "十一月";
+                    break;
+                case 12:
+                    result.first = "十二月";
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
         // GET: Index
         public ActionResult Index()
         {
@@ -15,11 +63,11 @@ namespace WebSite.Controllers
             var newsList = GetList<news>(6);
             var purchaseList = GetList<purchase>(6);
             var teamList = GetList<team>(12);
-                
-            ViewBag.experts = expertList;
-            ViewBag.newses = newsList;
-            ViewBag.purchases = purchaseList;
-            ViewBag.teams = teamList;
+              
+            ViewBag.experts = from record in expertList select new { image = record.expert_image,name = record.expert_name,introduction = record.expert_introduce };
+            ViewBag.newes = from record in newsList select new {name = record.news_title, time = GetMonthAndDay(record.news_time) };
+            ViewBag.purchases = from record in purchaseList select new { name = record.purchase_title, time = GetMonthAndDay(record.purchase_time) }; ;
+            ViewBag.teams = from record in teamList select new { name = record.team_name };
             return View();
         }
 
