e8686cdae1d3ba09847f9ccb00f907d909a60b1e
zgf <zgf_lawliet@foxmail.com>
2015-01-28 10:51:19 +0800

    add验证码

--- WebSite/Controllers/Module/ValidateCode.cs
+++ WebSite/Controllers/Module/ValidateCode.cs
@@ -74,63 +74,56 @@ namespace WebSite.Controllers.Module
                 }
                 return validateNumberStr;
             }
-            /// <summary>
-            /// 创建验证码的图片
-            /// </summary>
-            /// <param name="containsPage">要输出到的page对象</param>
-            /// <param name="validateNum">验证码</param>
-            public void CreateValidateGraphic(string validateCode,HttpContext containsPage)
+        public byte[] CreateValidateGraphic(string validateCode)
+        {
+            Bitmap image = new Bitmap((int)Math.Ceiling(validateCode.Length * 12.0), 22);
+            Graphics g = Graphics.FromImage(image);
+            try
             {
-                Bitmap image = new Bitmap((int)Math.Ceiling(validateCode.Length * 12.0), 22);
-                Graphics g = Graphics.FromImage(image);
-                try
+                //生成随机生成器
+                Random random = new Random();
+                //清空图片背景色
+                g.Clear(Color.White);
+                //画图片的干扰线
+                for (int i = 0; i < 25; i++)
                 {
-                    //生成随机生成器
-                    Random random = new Random();
-                    //清空图片背景色
-                    g.Clear(Color.White);
-                    //画图片的干扰线
-                    for (int i = 0; i < 25; i++)
-                    {
-                        int x1 = random.Next(image.Width);
-                        int x2 = random.Next(image.Width);
-                        int y1 = random.Next(image.Height);
-                        int y2 = random.Next(image.Height);
-                        g.DrawLine(new Pen(Color.Silver), x1, y1, x2, y2);
-                    }
-                    Font font = new Font("Arial", 12, (FontStyle.Bold | FontStyle.Italic));
-                    LinearGradientBrush brush = new LinearGradientBrush(new Rectangle(0, 0, image.Width, image.Height),
-                     Color.Blue, Color.DarkRed, 1.2f, true);
-                    g.DrawString(validateCode, font, brush, 3, 2);
-                    //画图片的前景干扰点
-                    for (int i = 0; i < 100; i++)
-                    {
-                        int x = random.Next(image.Width);
-                        int y = random.Next(image.Height);
-                        image.SetPixel(x, y, Color.FromArgb(random.Next()));
-                    }
-                    //画图片的边框线
-                    g.DrawRectangle(new Pen(Color.Silver), 0, 0, image.Width - 1, image.Height - 1);
-                    //保存图片数据
-                    MemoryStream stream = new MemoryStream();
-                    image.Save(stream, ImageFormat.Jpeg);
-                    //输出图片流
-                    containsPage.Response.Clear();
-                    containsPage.Response.ContentType = "image/jpeg";
-                    containsPage.Response.BinaryWrite(stream.ToArray());
+                    int x1 = random.Next(image.Width);
+                    int x2 = random.Next(image.Width);
+                    int y1 = random.Next(image.Height);
+                    int y2 = random.Next(image.Height);
+                    g.DrawLine(new Pen(Color.Silver), x1, y1, x2, y2);
                 }
-                finally
+                Font font = new Font("Arial", 12, (FontStyle.Bold | FontStyle.Italic));
+                LinearGradientBrush brush = new LinearGradientBrush(new Rectangle(0, 0, image.Width, image.Height),
+                 Color.Blue, Color.DarkRed, 1.2f, true);
+                g.DrawString(validateCode, font, brush, 3, 2);
+                //画图片的前景干扰点
+                for (int i = 0; i < 100; i++)
                 {
-                    g.Dispose();
-                    image.Dispose();
+                    int x = random.Next(image.Width);
+                    int y = random.Next(image.Height);
+                    image.SetPixel(x, y, Color.FromArgb(random.Next()));
                 }
+                //画图片的边框线
+                g.DrawRectangle(new Pen(Color.Silver), 0, 0, image.Width - 1, image.Height - 1);
+                //保存图片数据
+                MemoryStream stream = new MemoryStream();
+                image.Save(stream, ImageFormat.Jpeg);
+                //输出图片流
+                return stream.ToArray();
             }
-            /// <summary>
-            /// 得到验证码图片的长度
-            /// </summary>
-            /// <param name="validateNumLength">验证码的长度</param>
-            /// <returns></returns>
-            public static int GetImageWidth(int validateNumLength)
+            finally
+            {
+                g.Dispose();
+                image.Dispose();
+            }
+        }
+        /// <summary>
+        /// 得到验证码图片的长度
+        /// </summary>
+        /// <param name="validateNumLength">验证码的长度</param>
+        /// <returns></returns>
+        public static int GetImageWidth(int validateNumLength)
             {
                 return (int)(validateNumLength * 12.0);
             }
