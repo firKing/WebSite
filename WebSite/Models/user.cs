3d030180772e89ad082b49b78a837aa3f4dc7b80
zgf <zgf_lawliet@foxmail.com>
2015-01-31 19:35:52 +0800

    add 检测用户名存在

--- WebSite/Models/user.cs
+++ WebSite/Models/user.cs
@@ -7,6 +7,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.Web.Mvc;
+
 namespace WebSite.Models
 {
     using System;
@@ -30,8 +32,9 @@ namespace WebSite.Models
         [Required(ErrorMessage = "*")]
         [EmailAddress(ErrorMessage = "Please enter valid email")]
         public string user_mail { get; set; }
-        [Required(ErrorMessage = "*")]
-        [RegularExpression(@"^[\w\u4E00-\u9FA5][\u4E00-\u9FA5\w\d_]{0,19}$", ErrorMessage = "Please enter valid name.")]
+      //  [Required(ErrorMessage = "*")]
+      //  [RegularExpression(@"^[\w\u4E00-\u9FA5][\u4E00-\u9FA5\w\d_]{0,19}$", ErrorMessage = "Please enter valid name.")]
+        [Remote("CheckRegisterNameExist","Verify", AdditionalFields = "user_type", ErrorMessage = "user name is registered")]
         public string user_name { get; set; }
         [Required(ErrorMessage = "*")]
 
