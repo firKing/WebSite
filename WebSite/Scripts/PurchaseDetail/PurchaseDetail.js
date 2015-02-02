a967bc1d58547d4d0b7c6ddd33d0ed0d9360b707
evanna <evannalikeyou@gmail.com>
2015-02-02 11:49:18 +0800

    detail

--- WebSite/Scripts/PurchaseDetail/PurchaseDetail.js
+++ WebSite/Scripts/PurchaseDetail/PurchaseDetail.js
@@ -1,6 +1,9 @@
 ﻿window.onload = function isLogin() {
+    var user_type = $("#login").attr("title") || false;
+    if (user_type != "Vendor") {
+        $("a.btn").attr("href", "");
+    }
     $("button.btn").click(function() {
-        var user_type = $("#login").attr("value") || false;
         if (!user_type) {
             alert("请登录!");
         } else if (user_type != "Vendor") {
